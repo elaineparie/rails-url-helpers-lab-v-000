198345e0022e3f9b3f8b5cdaa475fc1bf5613b34
Elaine Anderson <eparie@bu.edu>
2018-07-31 19:16:52 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show]
 
-  get "/students/:id/activate", to: "students#activate", as: "activate_student", to: redirect("students/:id") ,
+  get "/students/:id/activate", to: "students#activate", as: "activate_student"
 
 
   # This show route can be refactored into the above resources method call, like so:
