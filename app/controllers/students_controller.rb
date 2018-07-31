f86a45739a3e529f907a0ca4e0df38800660fa32
Elaine Anderson <eparie@bu.edu>
2018-07-31 18:56:56 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,7 @@ class StudentsController < ApplicationController
   def activate
       @student = Student.find(params[:id])
       if !@student.active
+        binding.pry
       @student.active = true
     elsif @student.active = true
       @student.active = false
